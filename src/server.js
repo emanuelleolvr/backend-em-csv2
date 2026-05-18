438fa2fe0aadaeb0e8531e270cd3f5d9d2b0bd06
Emanuele <emanuele.pinezzi@aluno.senai.br>
2026-05-18 13:12:13 -0300

    Atualiza rota raiz com pesquisa por data

--- src/server.js
+++ src/server.js
@@ -20,14 +20,9 @@ app.get('/', (req, res) => {
     rotasDisponiveis: {
       listarTodasAsLeituras: 'GET /api/leituras',
       pesquisarLeiturasPorData: 'GET /api/leituras/data/2026-04-01',
-      pesquisarLeiturasImportadasPorCsv: 'GET /api/leituras/data/2026-04-02',
     },
     formatoDaData: 'YYYY-MM-DD',
-    exemplosDeUso: [
-      'http://localhost:3000/api/leituras',
-      'http://localhost:3000/api/leituras/data/2026-04-01',
-      'http://localhost:3000/api/leituras/data/2026-04-02',
-    ],
+    exemploDeUso: 'http://localhost:3000/api/leituras/data/2026-04-01',
   });
 });
 
